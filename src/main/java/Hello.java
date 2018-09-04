713ea9880a9694a062f529655ded8d654f3b8a35
邢西洋 <821548842@qq.com>
2018-09-04 09:29:52 +0800

    idea整合github

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @program: hello
+ * @description: 测试
+ * @author: XXY
+ * @create: 2018-09-04 09:24
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
